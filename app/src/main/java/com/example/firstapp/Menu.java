cfe47d51c2818a7859064f39b2da43eb3dbfca01
Astha <asthabhartiab84@gmail.com>
2024-04-21 23:58:33 +0530

    First Version

--- app/src/main/java/com/example/firstapp/Menu.java
+++ app/src/main/java/com/example/firstapp/Menu.java
@@ -44,28 +44,9 @@ public class Menu extends AppCompatActivity implements NavigationView.OnNavigati
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_menu);
 
-
-//        if (savedInstanceState == null) {
-//            getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, new HomeFragment()).commit();
-//            navigationView.setCheckedItem(R.id.nav_home);
-//        }
-
-
-
-        // to make the Navigation drawer icon always appear on the action bar
-
-//        if (getSupportActionBar() != null) {
-//            getSupportActionBar().hide();
-//        }
-//        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-//            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-//            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-//            return insets;
-//        });
         binding = ActivityMenuBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
         replaceFragment(new HomeFragment());
-
 
         binding.bottomNavigation.setOnItemSelectedListener(new NavigationBarView.OnItemSelectedListener() {
             @Override
@@ -98,26 +79,9 @@ public class Menu extends AppCompatActivity implements NavigationView.OnNavigati
                     replaceFragment(new ChatbotFragment());
                     return true;
                 }
-
-
                 return true;
             }
         });
-
-
-
-//        Toolbar toolbar = findViewById(R.id.toolbar); //Ignore red line errors
-//        setSupportActionBar(toolbar);
-//
-//        drawerLayout = findViewById(R.id.drawerLayout);
-//        NavigationView navigationView = findViewById(R.id.nav_view);
-//        navigationView.setNavigationItemSelectedListener(this);
-//        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(this, drawerLayout,toolbar, R.string.nav_open,
-//                R.string.nav_close);
-//        drawerLayout.addDrawerListener(toggle);
-//        toggle.syncState();
-//        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
-
 
     }
     private  void replaceFragment (Fragment fragment){
@@ -125,33 +89,12 @@ public class Menu extends AppCompatActivity implements NavigationView.OnNavigati
         FragmentTransaction transaction = fragmentManager.beginTransaction();
         transaction.replace(R.id.container, fragment);
         transaction.commit();
-//        new NavigationBarView(this);
-
-
-
     }
 
     @SuppressLint("NonConstantResourceId")
     @Override
     public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
-//        switch (menuItem.getItemId()) {
-//
-//            case R.id.nav_account:
-//                getSupportFragmentManager().beginTransaction().replace(R.id.container, new HomeFragment()).commit();
-//                break;
-//            case R.id.nav_settings:
-//                getSupportFragmentManager().beginTransaction().replace(R.id.container, new ChatbotFragment()).commit();
-//                break;
-////            case R.id.nav_share:
-////                getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, new ShareFragment()).commit();
-////                break;
-//            case R.id.nav_logout:
-//                getSupportFragmentManager().beginTransaction().replace(R.id.container, new AddFragment()).commit();
-//                break;
-////            case R.id.nav_logout:
-////                Toast.makeText(this, "Logout!", Toast.LENGTH_SHORT).show();
-////                break;
-//        }
+
         drawerLayout.closeDrawer(GravityCompat.START);
         return true;
 
